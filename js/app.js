b2eb4172a8e488169f1b025e213a5f328db9aac0
Catirau Mihail <catiraumihail@gmail.com>
2016-02-22 00:18:21 +0200

    Add navigation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
+var app = angular.module('QuizApp', ['ngRoute']);
 
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl:'views/home.html'
+        })
+        .when('/contact', {
+            templateUrl:'views/contact.html'
+        })
+        .when('/about', {
+            templateUrl:'views/contact.html'
+        })
+        .when('/edit', {
+            templateUrl:'views/edit.html'
+        })
+        .when('/new', {
+            templateUrl:'views/new.html',
+        })
+        .otherwise({
+            redirectTo:'/'
+        });
+});
